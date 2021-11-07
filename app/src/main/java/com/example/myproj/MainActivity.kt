b8d8cba44384fc66015ca0c6ccd0c91a8fba2fd5
stado-y <psybb228@gmail.com>
2021-11-07 18:28:21 +0200

    add MainActivityBtnSendEmail to MainActivity

--- app/src/main/java/com/example/myproj/MainActivity.kt
+++ app/src/main/java/com/example/myproj/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.myproj
 
-import android.support.v7.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.myproj.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity","MainActivity started")
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.MainActivityBtnSendEmail.setOnClickListener {
+            Log.d("MainActivity","btnShare clicked!")
+
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(resources.getString(R.string.MainActivityChooserEmail)))
+
+            val title = resources.getString(R.string.MainActivityChooserTitle)
+
+            val chooser = Intent.createChooser(intent, title)
+
+            try {
+                startActivity(chooser)
+            } catch (e: ActivityNotFoundException) {
+            }
+        }
+
+
+
     }
 }
